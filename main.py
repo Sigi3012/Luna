81f97f00b458d1bd77036510e959bbdf5d231ea6
Sigi3012 <50233886+Sigi3012@users.noreply.github.com>
2023-10-23 18:59:49 +0100

    toggle

--- main.py
+++ main.py
@@ -4,12 +4,31 @@ import json
 import re
 
 intents = discord.Intents.all()
-client = discord.client(intents=intents)
+client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 with open("config.json", "r") as file:
     config = json.load(file)
     print("loaded config")
     
+@tree.command(
+    name = "toggle",
+    description = "This enables or disables the fixer")
+@app_commands.checks.has_permissions(administrator=True)
+async def togglefixer(interaction):
+    config['enabled'] = not config['enabled']
+    with open('config.json', 'w') as f:
+        json.dump(config, f, indent=4)
+    await interaction.response.send_message(f"Toggled to {config['enabled']}", ephemeral=True)
+@tree.error
+async def on_app_command_error(interaction, error):
+    if isinstance(error, app_commands.MissingPermissions):
+        await interaction.response.send_message("You dont have permissions to do this!", ephemeral=True)
+    
+@client.event
+async def on_ready():
+    await tree.sync()
+    print("Online!")
+
 if __name__ == "__main__":    
     client.run(config["token"])
