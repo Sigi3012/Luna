038e79c8dc12092f5ca214af3e8e7bed72bd0fa6
Sigi3012 <50233886+Sigi3012@users.noreply.github.com>
2023-10-23 20:54:09 +0100

    shutdown command and logging

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import discord
 from discord import app_commands
 import json
+import time
 import re
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+def currentTime():
+    return time.strftime("%H:%M:%S", time.localtime())
+
 with open("config.json", "r") as file:
     config = json.load(file)
-    print("loaded config")
     
 @tree.command(
     name = "toggle",
@@ -20,15 +23,28 @@ async def togglefixer(interaction):
     with open('config.json', 'w') as f:
         json.dump(config, f, indent=4)
     await interaction.response.send_message(f"Toggled to {config['enabled']}", ephemeral=True)
+    print(f"Toggled to: {config['enabled']}@{currentTime()}")
 @tree.error
 async def on_app_command_error(interaction, error):
     if isinstance(error, app_commands.MissingPermissions):
         await interaction.response.send_message("You dont have permissions to do this!", ephemeral=True)
     
+@tree.command(name= "shutdown", description = "turns off the bot!")
+@app_commands.checks.has_permissions(administrator=True)
+async def shutdown(interaction):
+    await interaction.response.send_message("Shutting down...", delete_after=3.0, ephemeral=True)
+    print(f"Bot offline@{currentTime()}")
+    await client.close()
+@tree.error
+async def on_app_command_error(interaction, error):
+    if isinstance(error, app_commands.MissingPermissions):
+        await interaction.response.send_message("You dont have permissions to do this!", ephemeral=True)
+
 @client.event
 async def on_ready():
     await tree.sync()
-    print("Online!")
+    print(f"Online!@{currentTime()}")
+    print(f"Config loaded!@{currentTime()}")
 
 if __name__ == "__main__":    
     client.run(config["token"])
