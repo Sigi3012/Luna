0832601d7ba89925472e94c908510817c5f8ea56
Sigi3012 <50233886+Sigi3012@users.noreply.github.com>
2023-10-23 20:44:31 +0100

    Revert "added shutdown command and changed config loading"
    This reverts commit 7154885b1ce87821c7468517f3cee23f403c7f57.
    I am really stupid and put the config load within a function that can only be called while the bot is already running....

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import discord
 from discord import app_commands
 import json
-import time
 import re
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-def currentTime():
-    return time.strftime("%H:%M:%S", time.localtime())
-
+with open("config.json", "r") as file:
+    config = json.load(file)
+    print("loaded config")
+    
 @tree.command(
     name = "toggle",
     description = "This enables or disables the fixer")
@@ -25,26 +25,10 @@ async def on_app_command_error(interaction, error):
     if isinstance(error, app_commands.MissingPermissions):
         await interaction.response.send_message("You dont have permissions to do this!", ephemeral=True)
     
-@tree.command(name= "shutdown", description = "turns off the bot!")
-@app_commands.checks.has_permissions(administrator=True)
-async def shutdown(interaction):
-    await interaction.response.send_message("Shutting down...", delete_after=3.0, ephemeral=True)
-    print(f"Bot offline@{currentTime}")
-    await client.close()
-@tree.error
-async def on_app_command_error(interaction, error):
-    if isinstance(error, app_commands.MissingPermissions):
-        await interaction.response.send_message("You dont have permissions to do this!", ephemeral=True)
-
 @client.event
 async def on_ready():
     await tree.sync()
     print("Online!")
-    # Loads config
-    with open("config.json", "r") as file:
-        global config
-        config = json.load(file)
-        print("loaded config")
 
 if __name__ == "__main__":    
     client.run(config["token"])
