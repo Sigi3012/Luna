2b199e3621fbb8c328d693df2261df492785896c
Sigi3012 <50233886+Sigi3012@users.noreply.github.com>
2023-10-25 19:50:40 +0100

    pixiv embed and vm.tiktok

--- main.py
+++ main.py
@@ -42,20 +42,29 @@ async def on_message(message):
         return
     if not config["enabled"]:
         return
+    
     async def match(result):
         print("Match found")
         await message.reply(f"<@{message.author.id}>: {result}", silent=True)
         await asyncio.sleep(1e-3)
         await message.delete()
+        
     VXPATTERN = r'https?://(?:www\.)?(?:twitter\.com|x\.com)/([^/]+)/status/(\d+)(?:/photo/\d)?'
     VXREPLACEMENT = r'https://vxtwitter.com/\1/status/\2'
     DDPATTERN = r'https?://(?:www\.)?instagram\.com\/reel\/([a-zA-Z0-9_-]+)(\/\?igshid=[a-zA-Z0-9_-]+)?'
     DDREPLACEMENT = r'https://ddinstagram.com/reel/\1\2'
-    TTPATTERN = r'https:\/\/(?:www\.)?tiktok\.com\/@([^\/]+)\/video\/(\d+)'
+    TTPATTERN = r'https:\/\/(?:www\.|vm\.)?tiktok\.com\/@([^\/]+)\/video\/(\d+)'
     TTREPLACEMENT = r'https://vxtiktok.com/@\1/video/\2'
+    VMPATTERN = r'https:\/\/vm\.tiktok\.com\/([\[A-Za-z0-9]+)'
+    VMREPLACEMENT = r'https://vm.vxtiktok.com/\1'
+    PXPATTERN = r'https:\/\/(?:www\.)?pixiv\.net\/(?:en\/)?(?:artworks|member_illust)\/(\d+)'
+    PXREPLACEMENT = r'https://phixiv.net/artworks/\1'
+    
     endResult = re.sub(VXPATTERN, VXREPLACEMENT, message.content)
     endResult = re.sub(DDPATTERN, DDREPLACEMENT, endResult)
     endResult = re.sub(TTPATTERN, TTREPLACEMENT, endResult)
+    endResult = re.sub(VMPATTERN, VMREPLACEMENT, endResult)
+    endResult = re.sub(PXPATTERN, PXREPLACEMENT, endResult)
     if endResult != message.content:
         await match(endResult)
     
