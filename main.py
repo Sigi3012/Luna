3b4bd1617b5da4fa798c6955f9baf7aecae49cc1
Sigi3012 <50233886+Sigi3012@users.noreply.github.com>
2023-10-30 23:34:15 +0000

    idiot protection

--- main.py
+++ main.py
@@ -18,6 +18,17 @@ async def missingPermissions(interaction):
 with open("config.json", "r") as file:
     config = json.load(file)
     
+if config["admin"] is not None:
+    try:
+        test = int(config["admin"])
+    except ValueError:
+        print("Please set a user ID in config.json and run the bot again.")
+        exit()
+
+if config["token"] == "yourtokenhere":
+    print("Please set your discord bot token in the config.json, to get your bot token go to the discord developer portal and select your app, then it is found in Bot > Token.")
+    exit()
+   
 if config["firstRun"]:
     print("Thank you for using my bot!\nTo enable the fixer use '/toggle' (admin perms required)")
     config["firstRun"] = not config["firstRun"]
