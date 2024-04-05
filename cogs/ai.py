d2621b46ecdac30716c6b3492683ca43bfd6f128
Sigi3012 <50233886+Sigi3012@users.noreply.github.com>
2024-04-05 21:22:12 +0100

    Basic implementation of stable diffusion intergration

--- cogs/ai.py
+++ cogs/ai.py
@@ -5,20 +5,79 @@ from discord.ext import commands
 from main import Config
 from helpers.checks import missingPermissions
 
+import aiohttp
+import socket
+
+import os
+import base64
+
 from typing import Literal
 
 config = Config.getMainInstance()
 
-remoteHost: str = None
-secretKey: str = None
-enabled: bool = False
+remoteHost: str = "http://127.0.0.1:727"
+secretKey: str = "secret"
+enabled: bool = True
 channel: int = 0
+
+class ApiException(Exception):
+    def __init__(self, m):
+        self.message = m
+    def __str__(self):
+        return self.message
+
+async def fetchImage(payload):
+    connector = aiohttp.TCPConnector(family = socket.AF_INET)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "secret"
+    }
+    async with aiohttp.ClientSession(connector = connector, headers = headers) as session:
+        async with session.post(url = f"{remoteHost}/image", json = payload) as response:
+            if response.status == 200:
+                json = await response.json()
+                return json
+            else:
+                raise ApiException(f"{response.status}: Error while contacting api")
+
+def handleDecode(userId: int, response):
+    if not os.path.exists(path = f"./cache/{userId}"):
+        os.makedirs(f"./cache/{userId}")
+    
+    with open(f"./cache/{userId}/aiOutput.png", "wb") as file:
+        try:
+            file.write(base64.b64decode(response["images"][0]))
+            print("successfully wrote to file")
+        except Exception as e:
+            print(e)
 
 # --------- #
 
 class InputForm(ui.Modal, title = "Generate an image!"):
-    pass
+    prompt = ui.TextInput(label="Prompt", placeholder = "1girl, best_quality, ...", required = True)
+    negativePrompt = ui.TextInput(label = "Negative Prompt", placeholder = "bad quality, worst quality", required = False)
+    
+    async def on_submit(self, interaction: discord.Interaction):
+        print("submitted")
+        payload = {
+            "prompt": str(self.prompt),
+            "negativePrompt": str(self.negativePrompt)
+        }
+        
+        try:
+            await interaction.response.send_message(content = "Please wait..", ephemeral = True, delete_after = 5)
+            res = await fetchImage(payload)
+        except Exception as e:
+            await interaction.response.send_message(content = e, ephemeral = True)
+            return
 
+        handleDecode(interaction.user.id, res)
+
+        print(interaction.channel_id)
+        instance = await Ai.getInstance()
+        channel = await instance.getChannel(id = interaction.channel_id)
+        await channel.send(content = f"<@{interaction.user.id}>", file = discord.File(f"./cache/{interaction.user.id}/aiOutput.png"))
+        
 class SetupForm(ui.Modal, title = "Setup"):
     host = ui.TextInput(label="Host", placeholder = "127.0.0.1:3000", required = True)
     secret = ui.TextInput(label = "Secret key", placeholder = "2a7c...", required = True)
@@ -33,6 +92,15 @@ class SetupForm(ui.Modal, title = "Setup"):
 # --------- #
 
 class Ai(commands.Cog):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+            return cls._instance
+        else:
+            raise Exception("An instance of the client already exists.")
+    
     def __init__(self, client: commands.Bot):
         self.client = client
 
@@ -40,18 +108,27 @@ class Ai(commands.Cog):
     async def on_ready(self):
         print(f"Cog: {__name__} has loaded")
     
+    @classmethod
+    async def getInstance(cls):
+        if not cls._instance:
+            cls._instance = cls()
+        return cls._instance
+    
+    async def getChannel(self, id):
+        return self.client.get_channel(id)
+    
     generate = app_commands.Group(name = "generate", description = "Image generation")
     
     @generate.command(name = "image", description = "Generates an image!")
     async def generateImage(self, interaction: discord.Interaction):
-        raise NotImplementedError
+        await interaction.response.send_modal(InputForm())
 
     @generate.command(name = "setup")            
     async def setup(self, interaction: discord.Interaction):
         if interaction.user.id == int(config.admin):
             await interaction.response.send_modal(SetupForm())
         else:
-            await missingPermissions()
+            await missingPermissions(interaction)
     
     @generate.command(name = "enabled")    
     async def enabled(self, interaction: discord.Interaction, option: Literal["True", "False"]):
@@ -74,7 +151,7 @@ class Ai(commands.Cog):
                 enabled == False
                 await response()
         else:
-            await missingPermissions()
+            await missingPermissions(interaction)
             
     # Testing only will be removed in release
     @commands.is_owner()
